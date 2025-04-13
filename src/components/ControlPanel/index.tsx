98847388036e49f38bffcbafb367858ca06a5e30
Aruljothiviswa <aruljothi.viswa@gmail.com>
2025-04-13 17:02:16 +0800

    Control panel component updated

--- src/components/ControlPanel/index.tsx
+++ src/components/ControlPanel/index.tsx
@@ -1,5 +1,76 @@
+import "./ColorShapes.css";
+import { colors } from "../../const";
+import { constant } from "../../const";
+import { shapes } from "../../const";
+
 const ControlPanel = () => {
-  return <>This is ControlPanel</>;
+  const selectedTool = "shape";
+  const indexVal = 2;
+  if (!selectedTool) return null;
+
+  const onHandleShapeChange = (e: any) => {
+    console.log("onHandleShapeChange", e);
+  };
+
+  const onHandleColorChange = (e: any) => {
+    console.log("onHandleColorChange", e);
+  };
+
+  return (
+    <div style={{ border: "2px solid #615b5b", height: "130px" }}>
+      <h3>{constant.SHAPE_TOOL}</h3>
+      <div className="container">
+        <div style={{ marginRight: "25px" }}>
+          {constant.COLOR}
+          <br />
+          <div className="container">
+            {colors.map((color, index) => {
+              return (
+                <div className="shapes" key={index}>
+                  <div
+                    className="square color-box"
+                    style={{
+                      cursor: "pointer",
+                      background: `${color}`,
+                      margin: "5px",
+                      border: indexVal === index ? "2px solid Black" : "",
+                    }}
+                    onClick={() => onHandleColorChange(index)}
+                  ></div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {selectedTool === "shape" && (
+          <div className="container">
+            <div style={{ marginRight: "25px" }}>
+              {constant.SHAPE}
+              <br />
+              <div
+                className="container"
+                style={{ display: "flex", gap: "10px" }}
+              >
+                {shapes.map((shape, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      ...shape,
+                      cursor: "pointer",
+                      margin: "5px",
+                      backgroundColor:
+                        indexVal === index ? "#383332" : "lightgray",
+                    }}
+                    onClick={() => onHandleShapeChange(index)}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ControlPanel;
--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,24 @@ export const constant = {
   SHAPE_TOOL: "Shape Tool",
   FILL_COLOR: "Fill Color",
   CLEAR_ALL: "Clear All",
+  COLOR: "Color",
+  SHAPE: "Shape",
 };
+
+export const colors = ["red", "green", "yellow", "blue"];
+
+export const shapes = [
+  {
+    width: "20px",
+    height: "20px",
+    border: "1px solid black",
+    borderRadius: "50%",
+  }, // circle
+  { width: "20px", height: "20px", border: "1px solid black", borderRadius: 0 }, // square
+  {
+    width: "40px",
+    height: "20px",
+    border: "1px solid black",
+    borderRadius: "50%",
+  }, // oval
+];
